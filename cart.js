9f300d933215e4906c464ecdf448630f101f4fbe
prexi <prixedox@gmail.com>
2019-02-05 00:10:37 +0100

    add to cart and hover

--- cart.js
+++ cart.js
@@ -1,5 +1,16 @@
-const cartDiv = $('.cart');
-cartDiv.append('<p>');
-const cartP = $('.cart p');
-cartP.text('hi man');
-console.log(cartP);
+const items = $('.col.col-xs-12.col-md-6.col-lg-4');
+console.log(items);
+for (var i=0;i<items.length;i++){
+    const item = $('<a style="color:red;cursor:pointer;"></a>');
+    item.text('Add to cart');
+    items.eq(i).append(item);
+    item.on('click',addHandler);
+}
+
+function addHandler() {
+    console.log("hi");
+}
+
+//items.append('<p>');
+//const itemsP = $('.row .row div p');
+//console.log(itemsP);
