bd6fa8517229a8ac3b65ed95847ce1825b54a4aa
prexi <prixedox@gmail.com>
2019-02-05 23:52:09 +0100

    total value based on price

--- cart.js
+++ cart.js
@@ -1,5 +1,9 @@
 const items = $('.col.col-xs-12.col-md-6.col-lg-4');
-console.log(items);
+var cost = 0;
+
+const newP = $('<h2>Total: $0</h2>');
+$('.cart').append(newP);
+
 for (var i=0;i<items.length;i++){
     const item = $('<a style="color:red;cursor:pointer;"></a>');
     item.text('Add to cart');
@@ -8,7 +12,11 @@ for (var i=0;i<items.length;i++){
 }
 
 function addHandler() {
-    console.log("hi");
+    const parent = $(this).parent();
+    const costP = parent.find('p').eq(1).text();
+    cost += parseFloat(costP.replace("$",""));
+    console.log("Total: $" + cost);
+    newP.text("Total:  $" + Math.round(cost *100)/100);
 }
 
 //items.append('<p>');
